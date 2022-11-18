54d593bbc8f42bb4b4683b27ad187e644cfcd4e1
Lauren <laurenflynn@hsph.harvard.edu>
2022-11-18 01:06:50 -0500

    fixed description functio bugsn and fixed typos in saving panelpro output

--- ambry_info_restrictions.R
+++ ambry_info_restrictions.R
@@ -1,20 +1,27 @@
 set.seed(777)
 setwd("/Users/flynnlauren7/Desktop/Internships Summer 2022/Bayes Mendel/PedUtils/R")
 files.sources = list.files()
-sapply(files.sources, source)
+sapply(files.sources, source) #loads in all the PedUtils functions
+
+#load libraries
 library(dplyr)
+library(truncnorm)
+library(plyr)
+library(PanelPRO)
+library(ggplot2)
 
 
+#generate families
 families = list()
+probandIDS = c()
 #outputs = c()
-for (i in 1:10) {
+for (i in 1:500) {
   # Cancers
   cancers = "Colorectal"
   # Genes
-
   genes = "MLH1"
-
-  # Paternal aunts, paternal uncles
+  #family members
+  # Paternal aunts, pat ernal uncles
   nSibsPatern =floor(rtruncnorm(n=2, mean=3, 2))
   # Maternal aunts, maternal uncles
   nSibsMatern = floor(rtruncnorm(n=2, mean=3, 2))
@@ -31,6 +38,8 @@ for (i in 1:10) {
                       PanelPRODatabase, genes, cancers,
                       includeGeno = FALSE, includeBiomarkers = TRUE)
   famDF = as.data.frame(fam)
+  proband = famDF %>% filter(isProband==1)
+  probandIDS = c(probandIDS, proband$ID)
   # PanelPRO can be run on the simulated family
   #out = PanelPRO:::PanelPRO11(fam)
   families[[i]] = famDF
@@ -44,8 +53,8 @@ for (i in 1:10) {
 #### Proband info: sex, age, isDead==no, NA for riskMod and for interAge ###
 ambryProbandInfo <- function(ped){
   proband = ped %>% filter(isProband==1)
-  proband$riskmod <- NA
-  proband$interAge <- NA #should these be character(0)? what does that mean?
+  #proband$riskmod <- character(0)
+  #proband$interAge <- character(0) #should these be character(0)? what does that mean?
   proband$isDead <- 0
   return(proband)
 }
@@ -55,13 +64,13 @@ ambryProbandInfo <- function(ped){
 #restricting to first degree family
 #here genes is just MLH1
 ambryRelativeInfo <- function(ped){
-  #firstDegreeFam = firstDegreeFamilyMembers(ped)
   relatives = ped %>% filter(isProband==0)
   #print(relatives)
-  relatives$curAge <- NULL
+  relatives$CurAge <- NULL
   relatives$isDead <- NULL
-  relatives$riskMod <- NULL
-  relatives$interAge <- NULL
+  #relatives$riskMod <- character(0)
+  #relatives$interAge <- character(0)
+  #View(relatives)
   return(relatives)
 }
 
@@ -73,12 +82,14 @@ firstDegreeFamilyMembers <- function(ped){
     for(i in  1:nrow(ped)){
       relative=ped[i,]
       #siblings
-      if(!is.null(relative$MotherID) && !is.null(proband$MotherID) && !is.null(relative$FatherID) && !is.null(proband$FatherID) && (relative$MotherID==proband$MotherID) && (relative$FatherID==proband$FatherID)){
-        firstDegree <- rbind(firstDegree, relative)
-      }
-      #parents
-      else if(!is.null(proband$MotherID) && !is.null(proband$FatherID) && (relative$ID == proband$MotherID || relative$ID == proband$FatherID)){
-        firstDegree <-rbind(firstDegree,relative)
+      if(relative$ID != proband$ID){
+        if(!is.null(relative$MotherID) && !is.null(proband$MotherID) && !is.null(relative$FatherID) && !is.null(proband$FatherID) && (relative$MotherID==proband$MotherID) && (relative$FatherID==proband$FatherID)){
+          firstDegree <- rbind(firstDegree, relative)
+        }
+        #parents
+        else if(!is.null(proband$MotherID) && !is.null(proband$FatherID) && (relative$ID == proband$MotherID || relative$ID == proband$FatherID)){
+          firstDegree <-rbind(firstDegree,relative)
+        }
       }
     }
   }
@@ -88,26 +99,131 @@ firstDegreeFamilyMembers <- function(ped){
 
 #returns all affected family members and proband (affected or otherwise)
 affectedFamilyMembers <- function(ped){
-  #proband <- ped %>% filter(isProband==1)
-  #affectedFam = proband
+  proband <- ped %>% filter(isProband==1)
+  affectedFam = proband
   for(i in  1:nrow(ped)){
     relative=ped[i,]
-    if(relative$ID != proband$ID && relative$isAffAny!=0){
-      rbind(affectedFam, proband)
+    if( relative$ID != proband$ID && relative$isAffAny!=0){
+      rbind(affectedFam, relative)
     }
-
   }
+  return(affectedFam)
 }
 
 
-first <- firstDegreeFamilyMembers(families[[3]])
-View(first)
-View(families[[3]])
-ambryInfoFam <- ambryRelativeInfo(first)
-ambryInfoProband <- ambryProbandInfo(first)
-View(ambryInfoFam)
-ambryInfo <- rbind.fill(ambryInfoFam, ambryInfoProband)
-View(ambryInfo)
-affected <- affectedFamilyMembers(ambryInfo)
-View(affected)
+# first <- firstDegreeFamilyMembers(families[[3]])
+# # View(first)
+# 
+# ambryInfoFam <- ambryRelativeInfo(first)
+# ambryInfoProband <- ambryProbandInfo(first)
+# # View(ambryInfoFam)
+# ambryInfo <- rbind.fill(ambryInfoFam, ambryInfoProband)
+# # View(ambryInfo)
+# affected <- affectedFamilyMembers(ambryInfo)
+# View(affected)
+
+#filter down to first degree families
+firstDegreeFamilies = list()
+for(i in 1:length(families)){
+  first <- as.data.frame(firstDegreeFamilyMembers(families[[i]]))
+  firstDegreeFamilies[[i]] = first
+}
+
+
+#restrict to Ambry Info and affected family members only
+ambryInfoAffectedFamilies = list()
+for(i in 1:length(families)){
+  ambryInfoFam <- ambryRelativeInfo(firstDegreeFamilies[[i]])
+  ambryInfoProband <- ambryProbandInfo(firstDegreeFamilies[[i]])
+  ambryInfo <- rbind.fill(ambryInfoFam, ambryInfoProband)
+  affected <- affectedFamilyMembers(ambryInfo)
+  #if just the proband is left, take away the mother id and father id
+  proband <- affected %>% filter(isProband == TRUE)
+  if(!(proband$MotherID %in% affected$ID)){
+    proband$MotherID <- NA
+  }
+  if(!(proband$FatherID %in% affected$ID)){
+    proband$FatherID <- NA
+  }
+  ambryInfoAffectedFamilies[[i]] <- affected
+  
+}
+
+
+#### testing how many families have affected relatives
+affectedFamilies = 0 
+affectedFAMILIES = list()
+j = 1
+for(i in 1:length(families)){
+  f = firstDegreeFamilies[[i]]
+  f = f %>% filter(isAffAny==1) %>% filter(isProband==0)
+  if(nrow(f)>0){
+    affectedFamilies = affectedFamilies + 1
+    affectedFAMILIES[[j]] = f
+    j = j + 1
+  }
+}
+affectedFamilies #25 families have affected family members, 0 probands
+
+
+#generating outputs for first degree families with full information
+outputsFull <- list ()
+fullCarrierRisk <- list()
+for(i in 1:length(families)){
+  out = PanelPRO::PanelPRO(firstDegreeFamilies[[i]], , genes="MLH1", cancers="Colorectal")
+  id = as.character(probandIDS[i])
+  fullCarrierRisk[[i]] <- out$posterior.prob[[id]]$estimate[2]
+  outputsFull[[i]] = out#DF
+}
+
+#generating outputs for first degree families with Ambry information
+outputsAmbry <- list ()
+ambryRisks <- list()
+for(i in 1:length(families)){
+  out = PanelPRO::PanelPRO(ambryInfoAffectedFamilies[[i]], genes="MLH1", cancers="Colorectal")
+  id = as.character(probandIDS[i])
+  outRisk = out$posterior.prob[[id]]$estimate[2]
+  ambryRisks[[i]] = outRisk
+  #outDF = as.data.frame(out)
+  outputsAmbry[[i]] = out#DF
+}
+
+
+mlh1CarrierEstimatesFirstDegree <- outputsFull[[i]][["posterior.prob"]][["21"]]
+
+
+plot(fullCarrierRisk, ambryRisks)
+plot(ambryRisks, fullCarrierRisk)
+
+
+
+fullRisk <- unlist(fullCarrierRisk)
+ambryRisk <- unlist(ambryRisks)
+riskDF <- as.data.frame(cbind(ambryRisk,fullRisk))
+riskDF2 <- as.data.frame(cbind(ambryRisk,fullRisk, ambryInfoAffectedFamilies, firstDegreeFamilies))
+ggplot(data=riskDF) + geom_point(aes(x = ambryRisk, y = fullRisk)) + labs(x="Risks from Ambry Restricted Info", y="Risk from First Degree Families", title="Mapping Carrier Risk from Ambry Restricted Info to First Degree Family Information")
+                                                                      
+removed_outlier <- riskDF %>% filter(fullRisk < 0.2)
+
+outlier = riskDF2 %>% filter(fullRisk > 0.2)
+ggplot(data=removed_outlier) + geom_point(aes(x = ambryRisk, y = fullRisk)) + labs(x="Risks from Ambry Restricted Info", y="Risk from First Degree Families", title="Mapping Carrier Risk from Ambry Restricted Info to First Degree Family Information")
+
+#including outlier--linear regression
+summary(lm(riskDF$fullRisk~riskDF$ambryRisk))
+
+#excluding outlier--linear regression
+summary(lm(removed_outlier$fullRisk~removed_outlier$ambryRisk))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
