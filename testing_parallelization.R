ce285c4c6eab0c08a0bb85869b4e77da4362d880
Lauren Flynn <flynnlauren7@gmail.com>
2023-02-03 13:07:24 +0000

    parallelized family generation

--- testing_parallelization.R
+++ testing_parallelization.R
@@ -13,16 +13,25 @@ library(PanelPRO)
 library(tidyverse)
 library(MASS)
 library(parallel)
+library(foreach)
+library(doParallel)
 
 
 
 # Generate Families ----
-numberFamilies <- 100
+numberFamilies <- 10000
 families = list()
 probandIDS = c()
 probandMLH1Status = c()
 
-generateFamily <- function() {
+cores <- 48
+cl <- makeCluster(cores)
+registerDoParallel(cl)
+families <- foreach(i=1:numberFamilies, .packages =c("truncnorm","tidyverse")) %dopar%{
+  setwd("../PedUtils/R")
+  files.sources = list.files()
+  sapply(files.sources, source) #loads in all the PedUtils functions
+  setwd("../../thesis-work/")
   # Cancers
   cancers = "Colorectal"
   # Genes
@@ -58,10 +67,53 @@ generateFamily <- function() {
   probandMLH1Status = c(probandMLH1Status, proband$MLH1)
   # PanelPRO can be run on the simulated family
   #out = PanelPRO:::PanelPRO11(fam)
-  return(famDF)
+  families[[i]] = famDF
   #outputs = c(outputs, out)
   
 }
+
+
+# registerDoParallel(cl)
+# families <- foreach(i=1:numberFamilies, .packages="truncnorm") %dopar% {
+#   # Cancers
+#   cancers = "Colorectal"
+#   # Genes
+#   genes = "MLH1"
+#   #family members
+#   # Paternal aunts, paternal uncles
+#   nSibsPatern =floor(rtruncnorm(n=2, mean=3, 3))
+#   # Maternal aunts, maternal uncles
+#   nSibsMatern = floor(rtruncnorm(n=2, mean=3, 3))
+#   # Sisters and brothers
+#   nSibs = floor(rtruncnorm(n=2, mean=3, 3))
+#   # We make the assumption that the number of sons and daughters for the
+#   # proband and all siblings, is the same. Nieces and nephews of the proband
+#   # are not sampled separately.
+#   nGrandchild = floor(rtruncnorm(n=2, mean=6, 2))
+#   nChild = floor(rtruncnorm(n=2, mean=3, 2))
+#   
+#   # Simulate family using `PedUtils` code
+#   fam = sim.runSimFam(nSibsPatern, nSibsMatern, nSibs, nChild,
+#                       PanelPRODatabase, genes, cancers,
+#                       includeGeno = TRUE, includeBiomarkers = TRUE)
+#   famDF = as.data.frame(fam)
+#   for(j in 1:nrow(famDF)){
+#     if(famDF[j,]$isAffAny == 0){
+#       famDF[j,]$AgeAny = NA    
+#     }
+#     if(famDF[j,]$isAffCOL == 0){
+#       famDF[j,]$AgeCOL = NA    
+#     }
+#   }
+#   proband = famDF %>% filter(isProband==1)
+#   probandIDS = c(probandIDS, proband$ID)
+#   probandMLH1Status = c(probandMLH1Status, proband$MLH1)
+#   # PanelPRO can be run on the simulated family
+#   #out = PanelPRO:::PanelPRO11(fam)
+#   families[[i]] = famDF
+#   #outputs = c(outputs, out)
+#   
+# }
 
 
 # Functions to Filter Family Info ----
@@ -147,7 +199,7 @@ affectedFamilyMembers <- function(ped){
   return(affectedFam)
 }
 
-### ambry info both proband and relative ###
+### ambry info both proband and relative ---- 
 ambryInfo <- function(ped){
   proband <- ambryProbandInfo(ped)
   relatives <- ambryRelativeInfo(ped)
@@ -157,7 +209,7 @@ ambryInfo <- function(ped){
 }
 
 
-## function to make a binary list of proband MLH1 carrier status
+## function to make a binary list of proband MLH1 carrier status ----
 mlh1Probands <- function(ped){
   if(nrow(ped %>% filter(MLH1 ==1) %>% filter(isProband==1) > 0)){
     return(1)
@@ -170,13 +222,132 @@ mlh1Probands <- function(ped){
 
 
 
-# Run Filters in parallel ----
-cores <- 10
-cl <- makeCluster(cores)
+# Run Filters in parallel with mclapply ----
+#cores <- 10
+#cl <- makeCluster(cores)
 clusterExport(cl, c("families"))
 families <- mclapply(families, removeProbandStatus, mc.cores = cores, mc.preschedule=FALSE) #removes mlh1 status from probands
 firstDegree <- mclapply(families, firstDegreeFamilyMembers, mc.cores = cores, mc.preschedule=FALSE) #filters to first degree
 ambryFirstDegree <- mclapply(firstDegree, ambryInfo, mc.cores = cores, mc.preschedule = FALSE) #filters first degree info
 mlh1ProbandsTotal <- mclapply(mlh1StatusFamilies, mlh1Probands, mc.cores = cores, mc.preschedule = FALSE) #determines which families have mlh1
 stopCluster(cl)
+
+
+
+outputsFull <- list ()
+fullCarrierRisk <- list()
+considered_fams = families
+for(i in 1:numberFamilies){
+  out = PanelPRO::PanelPRO(families[[i]], genes="MLH1", cancers="Colorectal")
+  id = as.character(probandIDS[i])
+  fullCarrierRisk[[i]] <- out$posterior.prob[[id]]$estimate[2]
+  outputsFull[[i]] = out#DF
+}
+
+
+# #for first degree families
+outputsFirstDegree <- list ()
+firstDegreeCarrierRisk <- list()
+considered_fams = firstDegree
+for(i in 1:numberFamilies){
+  out = PanelPRO::PanelPRO(firstDegree[[i]], genes="MLH1", cancers="Colorectal")
+  id = as.character(probandIDS[i])
+  firstDegreeCarrierRisk[[i]] <- out$posterior.prob[[id]]$estimate[2]
+  outputsFirstDegree[[i]] = out#DF
+}
+outputsFirstDegreeAmbry <- list ()
+firstDegreeAmbryCarrierRisk <- list()
+#considers probands and their parents and affected family members
+for(i in 1:numberFamilies){
+  # browser()
+  out = PanelPRO::PanelPRO(ambryFirstDegree[[i]], genes="MLH1", cancers="Colorectal", debug = TRUE)
+  id = as.character(probandIDS[i])
+  firstDegreeAmbryCarrierRisk[[i]] <- out$posterior.prob[[id]]$estimate[2]
+  outputsFirstDegreeAmbry[[i]] = out#DF
+}
+
+affectedFamiliesLister <- function(f){
+  affectedFamiliesList = list()
+  for(i in 1:length(families)){
+    fam = as.data.frame(f[[i]])
+    fam = fam %>% filter(isAffAny==1) %>% filter(isProband==0)
+    if(nrow(fam) > 0){
+      affectedFamiliesList[[i]] = nrow(fam)
+    }
+    else{
+      affectedFamiliesList[[i]] = 0}
+  }
+  return(affectedFamiliesList)
+}
+
+mlh1ProbandsTotal <- c()
+probandMLH1 <- list()
+for(i in 1:length(families)){
+  if(nrow(mlh1StatusFamilies[[i]] %>% filter(MLH1 ==1) %>% filter(isProband==1) > 0)){
+    mlh1ProbandsTotal <- c(mlh1ProbandsTotal, i)
+    probandMLH1[[i]] = 1
+  } 
+  else{
+    probandMLH1[[i]] = 0
+  }}
+
+# Generate a summary table ----
+summaryTableMLH1 <- cbind(format(unlist(fullCarrierRisk), scientific=FALSE), unlist(firstDegreeCarrierRisk), unlist(firstDegreeAmbryCarrierRisk), unlist(affectedFamiliesLister(families)), unlist(affectedFamiliesLister(firstDegree)), unlist(probandMLH1))
+
+summaryTableMLH1 <- as.data.frame(summaryTableMLH1)
+
+names(summaryTableMLH1) <- c("Carrier Risk for Full Families", "Carrier Risk for First Degree Families", "Carrier Risk for Ambry Info", "Affected Individuals Full Familes", "Affected Individuals First Degree Families", "True MLH1 Carriers in Probands")
+
+print(summaryTableMLH1)
+
+
+summaryTable <- cbind(1:numberFamilies, summaryTableMLH1)
+names(summaryTable) <- c("famNumber", "carrierRiskFullFamilies", "carrierRiskFirstDegreeFamilies", "carrierRiskAmbryInfo", "affectedIndividualsFullFamilies", "affectedIndividualsFirstDegreeFamilies", "probandsMLH1")
+summaryTable
+
+summaryTable$carrierRiskFullFamilies <- as.numeric(summaryTable$carrierRiskFullFamilies)
+summaryTable$carrierRiskAmbryInfo <- as.numeric(summaryTable$carrierRiskAmbryInfo)
+summaryTable$carrierRiskFirstDegreeFamilies <- as.numeric(summaryTable$carrierRiskFirstDegreeFamilies)
+ggplot(data= summaryTable) +  geom_point(aes(x=famNumber, y=carrierRiskFullFamilies), color="Red") + 
+  geom_point(aes(x=famNumber, y= carrierRiskFirstDegreeFamilies), color="Blue") + 
+  geom_point(aes(x=famNumber, y=carrierRiskAmbryInfo), color="Green") +
+  #geom_point(aes(x=famNumber, y=probandsMLH1), color = "Purple") +
+  scale_y_log10() +
+  labs(title= "MLH1 Carrier Risk", x= "Family ID", y="Carrier Risk")
+
+
+ggplot(data= summaryTable) +  geom_point(aes(x=famNumber, y=carrierRiskFullFamilies, color="Carrier Risk for Full Families")) + 
+  geom_point(aes(x=famNumber, y= carrierRiskFirstDegreeFamilies, color="Carrier Risk for First Degree Families")) + 
+  geom_point(aes(x=famNumber, y=carrierRiskAmbryInfo, color="Carrier Risk Given Ambry Info")) +
+  #geom_point(aes(x=famNumber, y=probandsMLH1, color = "Probands with MLH1")) +
+  scale_y_log10() +
+  labs(title= "MLH1 Carrier Risk", x= "Family ID", y="Carrier Risk (log 10 scale)")
+
+
+# Median polish ----
+fitMedianPolished <- rlm(carrierRiskFullFamilies ~ carrierRiskAmbryInfo, data = summaryTable)
+summary(fitMedianPolished)
+
+# finally, plot the data and the fitted curve
+ggplot(data = summaryTable, aes(x= carrierRiskAmbryInfo, y= carrierRiskFullFamilies)) +
+  geom_point() + 
+  geom_line(aes(y = predict(fitMedianPolished)))+
+  labs(x = "Ambry Carrier Risk First Degree Families", y= "Carrier Risk for Full Family Information", title = "Comparing Carrier Risk for Ambry and Full Family Information with Loess Regression")
+
+
+
+
+# Loess ---- 
+# next, use the loess() function to fit a model
+fitLoess <- loess(carrierRiskFullFamilies ~ carrierRiskAmbryInfo, data = summaryTable, span = 0.2)
+summary(fitLoess) #RSE is 0.00397 (seems good but I'm not sure)
+
+# finally, plot the data and the fitted curve
+ggplot(data = summaryTable, aes(x= carrierRiskAmbryInfo, y= carrierRiskFullFamilies)) +
+  geom_point() + 
+  geom_line(aes(y = predict(fitLoess)))+
+  labs(x = "Ambry Carrier Risk First Degree Families", y= "Carrier Risk for Full Family Information", title = "Comparing Carrier Risk for Ambry and Full Family Information with Loess Regression") 
+
+
+
 
