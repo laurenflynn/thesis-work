e619449f484ad05f15cd3d0117151f9dcb8ce9c8
Lauren Flynn <flynnlauren7@gmail.com>
2023-02-16 21:29:28 +0000

    set allele freq to 0.2

--- updating_allele_freq.R
+++ updating_allele_freq.R
@@ -1,6 +1,6 @@
 load("~/PanelPRO/data/PanelPRODatabase.rda")
-PanelPRODatabase$AlleleFrequency[["MLH1_anyPV",1]] <- 0.5
-PanelPRODatabase$AlleleFrequency[["MLH1_anyPV",2]] <- 0.5
-PanelPRODatabase$AlleleFrequency[["MLH1_anyPV",3]] <- 0.5
+PanelPRODatabase$AlleleFrequency[["MLH1_anyPV",1]] <- 0.2
+PanelPRODatabase$AlleleFrequency[["MLH1_anyPV",2]] <- 0.2
+PanelPRODatabase$AlleleFrequency[["MLH1_anyPV",3]] <- 0.2
 View(PanelPRODatabase$AlleleFrequency)
 save(PanelPRODatabase, file = "~/PanelPRO/data/PanelPRODatabase.rda")
